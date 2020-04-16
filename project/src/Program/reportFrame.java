026be7fe0f95c2a5b95c9bbcaa54208145248a25
Anish Agrawal <anishagrawal.2612@gmail.com>
2020-04-16 13:57:30 +0530

    start making tables

---
+++ project/src/Program/reportFrame.java
@@ -0,0 +1,9 @@
+package Program;
+import javax.swing.*;
+public class reportFrame {
+	private JFrame report = new JFrame("Report Card");
+	public reportFrame() {
+		report.setSize(300,300);
+		report.setVisible(true);
+	}
+}
